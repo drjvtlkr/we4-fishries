7a8fb4e7dfeaf25e084780b9e300e00dd173dd41
drjvtlkr <dheerajrvithalkar@gmail.com>
2023-11-17 14:50:47 +0530

    some chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import "./App.css";
-import React from "react";
+import './App.css'
+import React from 'react'
 
 function App() {
+
   return (
     <>
-      <h1>Hello world</h1>
+<h1>Hello world</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
